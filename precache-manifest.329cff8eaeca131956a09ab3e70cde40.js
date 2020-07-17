25e7040ece199fafe790caba36173e43d0cf222d
Robert Perillo <perillo.robert@gmail.com>
2020-07-17 13:49:45 -0600

    Updates

---
+++ precache-manifest.329cff8eaeca131956a09ab3e70cde40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d24f9b976a489e31ec0b95703a8041",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2b74ff26b45b7352c6d9",
+    "url": "/project-4-assessment/static/css/main.48d4c0cc.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b74ff26b45b7352c6d9",
+    "url": "/project-4-assessment/static/js/main.b1530425.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
